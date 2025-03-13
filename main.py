9b22a21b0792523ce4ca62a7406430d800b6903c
Mohammad Adil <1681adilmohammad@gmail.com>
2025-03-13 21:46:11 +0530

    grid space for robot location added

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 
 name = input("What is the name of your robot?: ")
 id = int(input("Please provide the robot id: "))
+grid_size = 10
+row = int(input("Enter the row-coordinate of the robot: "))
+col = int(input("Enter the column-coordinate of the robot: "))
 
-print(f"Hello. My name is {name}. My ID is {id}.")
+
+if row<0:
+    row = 0
+if row > grid_size -1 :
+    row = grid_size - 1
+if col<0:
+    col = 0
+if col > grid_size -1 :
+    col = grid_size - 1
+
+def quadrant(row,col,grid_size):
+    mid = grid_size//2 - 1
+
+    if row<=mid and col <=mid:
+        return "Top-left quadrant"
+    elif row<=mid and col>mid:
+        return "Top-right quadrant"
+    elif row>mid and col<=mid:
+        return "Bottom-left quadrant"
+    elif row>mid and col>mid:
+        return "Bottom-right quadrant"
+    
+    return "Space"
+    
+loc = quadrant(row,col,grid_size)
+
+print(f"Hello. My name is {name}. My ID is {id}. My current location is {(row,col)}.I am in the {loc}" )
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(f"Hello. My name is {name}. My ID is {id}. My current location is {(row,col)}.I am in the {print(quadrant(row,col,grid_size))}" )

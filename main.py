f4d5d0b4dcbc79459a10c55517a9b391b43149ec
Mohammad Adil <1681adilmohammad@gmail.com>
2025-03-14 00:10:34 +0530

    Robot moving one step in N/S/E/W direction added

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-
-name = input("What is the name of your robot?: ")
-id = int(input("Please provide the robot id: "))
-grid_size = 10
-row = int(input("Enter the row-coordinate of the robot: "))
-col = int(input("Enter the column-coordinate of the robot: "))
-
-
-if row<0:
-    row = 0
-if row > grid_size -1 :
-    row = grid_size - 1
-if col<0:
-    col = 0
-if col > grid_size -1 :
-    col = grid_size - 1
-
 def quadrant(row,col,grid_size):
     mid = grid_size//2 - 1
 
@@ -28,8 +11,67 @@ def quadrant(row,col,grid_size):
         return "Bottom-right quadrant"
     
     return "Space"
-    
-loc = quadrant(row,col,grid_size)
 
-print(f"Hello. My name is {name}. My ID is {id}. My current location is {(row,col)}.I am in the {loc}" )
+
+def take_input():
+
+    name = input("What is the name of your robot?: ")
+    id = int(input("Please provide the robot id: "))
+    grid_size = 10
+    row = int(input("Enter the row-coordinate of the robot: "))
+    col = int(input("Enter the column-coordinate of the robot: "))
+    dirc = input("What is its initial direction [n|s|e|w]?: ")
+
+    if row<0:
+        row = 0
+    if row > grid_size -1 :
+        row = grid_size - 1
+    if col<0:
+        col = 0
+    if col > grid_size -1 :
+        col = grid_size - 1
+    
+    return name, id, grid_size, row, col, dirc
+
+def moving(row,col,grid_size,dirc):
+
+    if dirc=='n':
+        print("I am facing North")
+        if row!=0:
+            row = row - 1
+    elif dirc=='s':
+        print('I am facing South')
+        if row!=grid_size-1:
+            row = row + 1
+    elif dirc=='e':
+        print('I am facing East')
+        if col != grid_size-1:
+            col += 1
+    else:
+        print("I am facing West")
+        if col!=0:
+            col -= 1
+    print("Moving one step forward.")
+    return row,col
+
+
+
+
+def main():
+    
+    name, id, grid_size, row, col, dirc = take_input()
+
+    loc = quadrant(row,col,grid_size)
+    print(f"Hello. My name is {name}. My ID is {id}.")
+    print(f"My current location is {(row,col)}.I am in the {loc}")
+    row,col = moving(row,col,grid_size,dirc)
+    loc = quadrant(row,col,grid_size)
+    print(f"My current location is {(row,col)}.I am in the {loc}")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
 
